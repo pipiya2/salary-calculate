e3ffbb7a03613d9641933ae37fc3b9f628604796
강화광 <pipiya2@naver.com>
2023-07-06 17:33:34 +0900

    레이아웃작업

--- js/calculate_new.js
+++ js/calculate_new.js
@@ -1,3 +1,19 @@
 // $(document).ready(()=>{
 //     $(document).on(si)
 // })
+
+
+
+
+let count = 0;
+$(".add-btn-area").click(()=>{
+    let input = document.createElement('input');
+    input.setAttribute('type','text');
+    input.setAttribute('class','list');
+    $(input).val(`새로운 항목${count}`);
+    $("#list-area").append(input);
+    $(input).focus();
+    $(input).select();
+
+    // $(".list:last-child").focus(); -> 마지막요소 가져오는거
+})
