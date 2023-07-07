6eb7d34361ed1632e0d3006435c4fbe5117fd3dc
강화광 <pipiya2@naver.com>
2023-07-08 01:10:05 +0900

    레이아웃

--- js/calculate_new.js
+++ js/calculate_new.js
@@ -14,10 +14,10 @@ let dafaultFrame =
     </div>
     <div class = "write-list-area">
         <div class = "border-right">
-            <input type = "text">
+            <input type = "text" class = "change-flag list-memo">
         </div>
         <div>
-            <input type = "text" style = "text-align:right;">
+            <input type = "text" class = "change-flag list-number" style = "text-align:right;">
         </div>
     </div>
 </div>`;
@@ -50,9 +50,11 @@ let list = `
 let savedListFrame = `<div class = "list saved"></div>`;
 
 $(".add-btn").click((e)=>{
+    let tagName = e.target.tagName;
+    let target = tagName == "path" ? $(e.target).parent() : e.target;
 
     // 클릭했을때 disabled 클래스가 없으면 addList 실행
-    if(!$(e.target).hasClass('disabled')){
+    if(!$(target).hasClass('disabled')){
         disabledAddBtn(); // 추가버튼을 바로 비활성화 시켜줌.
         addList();
         activeSaveBtn(); // 저장버튼을 활성화 시켜줌.
@@ -82,9 +84,93 @@ $(document).on('click','.save-btn',(e)=>{
 //     }
 // })
 
+$(document).on('keyup','.change-flag',(e)=>{
+    let test = "";
+    let id = $('.selected')[0].id;
+
+    for(let i = 0 ; i< $('.change-flag').length; i++){
+        test += $('.change-flag')[i].value + "";
+    }
+
+    if(test == ""){
+        disabledAddBtn();
+        localStorage.removeItem(id);
+    }else{
+        activeAddBtn();
+        save(id);
+    }
+})
+
 function listConfirm(id){
     let listName = $('#'+id).val() == "" ? "새로운 항목" : $('#'+id).val();
-    $("#"+id).replaceWith(`<div class = "list saved">${listName}</div>`);
+    $("#"+id).replaceWith(`<div class = "list saved" id = ${id}>${listName}</div>`);
+}
+
+//#region 항목추가
+function addList(){
+    $('.saved').removeClass('selected');
+    let div = document.createElement('div');
+    // input.setAttribute('type','text');
+    div.setAttribute('class','list saved selected');
+    let id = getNextId();
+    div.setAttribute('id',id);
+    $(div).html('새로운 항목');
+    $("#list-area").append(div);
+    // $(input).focus();
+    // $(input).select();
+    showRightSection(id);
+}
+//#endregion
+
+//#region 내역 금액 적는란 활성화
+function showRightSection(id){
+    let obj = localStorage.getItem(id);
+
+    if(obj){
+        //있으면
+        $('.right-wrapper').append(header);
+    }else{
+        //id가 없으면 새로 추가하는 것으로 판단
+        $('.right-wrapper').append(dafaultFrame);
+    }
+}
+//#endregion
+
+$(document).on('click','.saved',e=>{
+    if($(e.target).hasClass('selected')){
+        return;
+    }
+    $('.saved').removeClass('selected');
+    $(e.target).addClass('selected');
+})
+
+//#region 다음 ID 채번
+function getNextId(){
+    let keyList = Object.keys(localStorage);
+    let key = defaultKeyNum;
+
+    return keyList.length == 0 ?  key : Number(keyList.sort((a,b) => b-a)[0]) + 1;
+}
+//#endregion
+
+function save(id){
+    let listName = $('#'+id)[0].innerHTML;
+    // listConfirm(id);
+
+    let listLength = $('.list-memo').length;
+
+    let obj = {
+        name : listName,
+        data : []
+    };
+
+
+    for(let i = 0; i<listLength; i++){
+        obj.data.push($('.list-memo')[i].value);
+        obj.data.push($('.list-number')[i].value);
+    }
+    
+    localStorage.setItem(id,JSON.stringify(obj));
 }
 
 //#region 저장버튼 활성화, 비활성화
@@ -102,62 +188,7 @@ function disabledAddBtn(){
     $('.add-btn').addClass('disabled');
 }
 
-function disabledAddBtn(){
+function activeAddBtn(){
     $('.add-btn').removeClass('disabled');
 }
 //#endregion
-
-//#region 항목추가
-function addList(){
-    let input = document.createElement('input');
-    input.setAttribute('type','text');
-    input.setAttribute('class','list writing');
-    input.setAttribute('id',getNextId());
-    $(input).val(`새로운 항목`);
-    $("#list-area").append(input);
-    $(input).focus();
-    $(input).select();
-    showRightSection();
-}
-//#endregion
-
-//#region 내역 금액 적는란 활성화
-function showRightSection(id){
-
-    let obj = localStorage.getItem(id);
-
-    if(obj){
-        //있으면
-        $('.right-wrapper').append(header);
-    }else{
-        //id가 없으면 새로 추가하는 것으로 판단
-        $('.right-wrapper').append(dafaultFrame);
-    }
-}
-//#endregion
-
-//#region 다음 ID 채번
-function getNextId(){
-    let keyList = Object.keys(localStorage);
-
-    let key = defaultKeyNum;
-    
-    return keyList == null ?  key : Number(keyList.sort((a,b) => b-a)[0]) + 1;
-}
-//#endregion
-
-function save(){
-    let id = $('.writing')[0].id;
-    console.log(id);
-    listConfirm(id);
-
-    let listLength = $('.list-memo').length;
-    let listName = $('#'+id)
-    let obj = {};
-
-    for(let i = 0; i<listLength; i++){
-        
-        console.log($('.list-memo')[i].val());
-        console.log($('.list-number')[i].val());
-    }
-}
