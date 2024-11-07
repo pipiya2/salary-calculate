b050604a8e409538f0f01e26331a176c24de39db
강화광 <t001143@daeduck.com>
2024-11-07 10:14:04 +0900

    sorting 에러 수정

--- js/calculate_new.js
+++ js/calculate_new.js
@@ -298,7 +298,7 @@ function clickTop(){
 //#region 저장된 항목 전부 load
 function loadAllList(){
     let keys = Object.keys(localStorage);
-    keys = keys.sort();
+    keys = keys.sort((a,b)=>a-b);
     keys.forEach(e=>{
         if(!(e.length > 3)){
             let obj = JSON.parse(localStorage.getItem(e));
